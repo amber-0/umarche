d18be188fe3c0a3e621998cc734c29fa62861316
ogino <ogino.takahiro.78m@yahoo.ne.jp>
2023-06-30 16:24:46 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,5 @@
   <x-slot name="header">ヘッダー</x-slot>
   コンポーネントテスト1
 
-  <x-tests.card title="タイトル1" content="内容1"></x-tests.card>
+  <x-tests.card title="タイトル1" content="内容1" :message="$message"></x-tests.card>
 </x-tests.app>
