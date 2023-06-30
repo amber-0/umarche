c0105b4033d3b5c8af455ff9d9f6f9a7d368fc5b
ogino <ogino.takahiro.78m@yahoo.ne.jp>
2023-06-30 16:54:28 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,6 @@
 
   <x-tests.card title="タイトル1" content="内容1" :message="$message"></x-tests.card>
   <x-tests.card title="タイトル1" ></x-tests.card>
+  <x-tests.card title="cssを変更したい" class="bg-red-300" ></x-tests.card>
+
 </x-tests.app>
