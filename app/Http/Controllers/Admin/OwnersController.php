2f0a9c6ef6b88f81544840ef51f592bf5b12d9cf
ogino <ogino.takahiro.78m@yahoo.ne.jp>
2023-07-05 19:57:28 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -22,15 +22,16 @@ class OwnersController extends Controller
      */
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now->year;
-        echo $date_parse; 
+    //     $date_now = Carbon::now();
+    //     $date_parse = Carbon::parse(now());
+    //     echo $date_now->year;
+    //     echo $date_parse; 
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
 
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
 
     }
 
@@ -39,7 +40,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
